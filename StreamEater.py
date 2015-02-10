146494ab3124cf4adf1d8dfa31eba666700a897c
Mark Stanford <m.stanford@samsung.com>
2015-02-10 14:45:08 -0800

    Initial commit

---
+++ StreamEater.py
@@ -0,0 +1,39 @@
+__author__ = 'm.stanford'
+
+import cv2
+import numpy as np
+
+
+#cap = cv2.VideoCapture()
+#cap.open('rtmp://flash.oit.duke.edu/vod/_definst_/test/Wildlife2.flv')
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+
+    lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+    for rho, theta in lines[0]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+        cv2.line(gray, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    # Display the resulting frame
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
